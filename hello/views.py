190c8debedfc51f5704107422536047031a84174
austinkwok1997 <austin.kwok@protonmail.com>
2020-10-21 18:24:58 -0700

    first commit

--- hello/views.py
+++ hello/views.py
@@ -3,11 +3,38 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+import argparse
+import os
+import pickle
+import pandas as pd
+#import matplotlib.pyplot as plt
+import numpy as np
+from .picker import NBAPicker
+import json
+
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    filename = "nba-player.csv"
+    with open(os.path.join(".","staticfiles", filename), "rb") as f:
+        ratings = pd.read_csv(f,names=("player","salary","team","position","team-agaisnt","ceiling","floor","points"))
 
+    model = NBAPicker()
+    team = model.pick(ratings)
+    result = team.to_json()
+    parsed = json.loads(result)
+    return render(request, "index.html", {"result": parsed})
+
+def pickTeam(request):
+    filename = "nba-player.csv"
+    with open(os.path.join(".","staticfiles", filename), "rb") as f:
+        ratings = pd.read_csv(f,names=("player","salary","team","position","team-agaisnt","ceiling","floor","points"))
+
+    model = NBAPicker()
+    team = model.pick(ratings)
+    result = team.to_json()
+    parsed = json.loads(result)
+    return HttpResponse( json.dumps( parsed ))
 
 def db(request):
 
